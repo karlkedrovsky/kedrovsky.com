642acb90fd1eec9f73b980cd8742a9fe3f630edc
Karl Kedrovsky <karl@kedrovsky.com>
2011-06-25 20:21:23 -0500

    added a blog

--- htdocs/sites/www.kedrovsky.com/themes/kedrovsky/page.tpl.php
+++ htdocs/sites/www.kedrovsky.com/themes/kedrovsky/page.tpl.php
@@ -12,13 +12,16 @@
   <div class="clear"></div>
   <div id="content" class="<?php print ($page['sidebar']) ? 'grid_8' : 'grid_12'; ?>">
     <?php print $messages; ?>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?><header><h1><?php print $title; ?></h1></header><?php endif; ?>
-    <?php print render($title_suffix); ?>
+    <?php if (!$is_front): ?>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?><header><h1><?php print $title; ?></h1></header><?php endif; ?>
+      <?php print render($title_suffix); ?>
+    <?php endif; ?>
     <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
     <?php print render($page['help']); ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php print render($page['content']); ?>
+    <?php if ($page['content_bottom']): print render($page['content_bottom']); endif; ?>
     <?php print $feed_icons; ?>
   </div>
   <?php if ($page['sidebar']): ?>
